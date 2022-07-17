f7c9667936227f80e4bb674ba1c2161cd3f1ea1c
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-07-17 21:50:21 +0100

    destructured version

--- src/components/Greeter.tsx
+++ src/components/Greeter.tsx
@@ -4,8 +4,13 @@ interface GreeterProps {
   person: string;
 }
 
-function Greeter(props: GreeterProps): JSX.Element {
-  return <div>Hello, {props.person}</div>;
+// function Greeter(props: GreeterProps): JSX.Element {
+//   return <div>Hello, {props.person}</div>;
+// }
+
+// a destructured version of the above
+function Greeter({ person }: GreeterProps): JSX.Element {
+  return <div>Hello, {person}</div>;
 }
 
 export default Greeter;
